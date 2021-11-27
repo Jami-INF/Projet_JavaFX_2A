8918f57723b8bf2d3de80c8fe30845f25a847973
vimartin12 <vincent.martin2@etu.uca.fr>
2021-11-27 11:09:00 +0100

    creation sous dossier mcd et ajout des DiagrammeDeClasse.mdj et DiagrammeCasUtilisation.mdj

---
+++ project/src/launch/Launch.java
@@ -0,0 +1,23 @@
+package launch;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class Launch extends Application {
+    public void start(Stage stage) throws IOException {
+        Parent root = (Parent) FXMLLoader.load(getClass().getClassLoader().getResource("../ressource/fxml/mario.fxml"));
+        Scene scene = new Scene(root, 400, 400);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
