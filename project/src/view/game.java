25ba316caea23a2c0d32181108fab14546318f9f
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2022-01-18 19:27:40 +0100

    Ajout temps de jeu bindé

--- project/src/view/game.java
+++ project/src/view/game.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.BoundingBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -13,6 +14,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.Parent;
+
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Timer;
@@ -22,12 +25,12 @@ import javafx.event.EventHandler;
 import javafx.geometry.VPos;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
 import controller.*;
 import model.Joueur;
 import model.*;
 import javafx.animation.AnimationTimer;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class game {
     static Scene s;
@@ -40,104 +43,114 @@ public class game {
     int tmpDureeSaut;
 
     public void startgame(Stage stage) {
-        Group g = new Group();
-        s = new Scene(g,1280,720);
-        stage.setScene(s);
-        collision collisionController = new collision();
-        actionClavier action = new actionClavier();
-        mouvement mouvementJoueur = new mouvement();
+            Group g = new Group();
+            s = new Scene(g,1280,720);
+            stage.setScene(s);
+            collision collisionController = new collision();
+            actionClavier action = new actionClavier();
+            mouvement mouvementJoueur = new mouvement();
 
-        //ActionMouvement(mouvementJoueur);
-        ////BACKGROUND
-        Image background = new Image (getClass().getClassLoader().getResource("image/background.jpg").toExternalForm());
-        ImageView backgroundIV = new ImageView (getClass().getClassLoader().getResource("image/background.jpg").toExternalForm());
-        g.getChildren().add(backgroundIV);
-        ////////////
+            //ActionMouvement(mouvementJoueur);
+            ////BACKGROUND
+            //Image background = new Image (getClass().getClassLoader().getResource("image/background.jpg").toExternalForm());
 
-        ////JOUEUR
-        Image Skin = new Image(getClass().getClassLoader().getResource("image/testpers.png").toExternalForm());
-        Joueur j1 = new Joueur(Skin, 10, 10, 10, 0, 0,"Joueur1");
+            ImageView backgroundIV = new ImageView (getClass().getClassLoader().getResource("image/background.jpg").toExternalForm());
+            g.getChildren().add(backgroundIV);
+            ////////////
 
-
-        //ImageView joueurIV = new ImageView(getClass().getClassLoader().getResource("image/testpers.png").toExternalForm());
-        ////////////
-
-        //Image bg = new Image(getClass().getClassLoader().getResource("image/background.png").toExternalForm());
-
-        //s.setFill(Color.grayRgb(35));
-        ArrayList<Plateforme> plateformeArrayList = new ArrayList<Plateforme>();
-        ArrayList<Entite> entites = new ArrayList<Entite>();
-
-        Image Platform = new Image(getClass().getClassLoader().getResource("image/platform.png").toExternalForm());
-        Plateforme p1 = new Plateforme(Platform, 100, 600, 100, 100, 200, true);
-        Plateforme p2 = new Plateforme(Platform, 100, 600, 100,300, 500,true);
-        Plateforme p3 = new Plateforme(Platform, 100, 600, 100, 500, 500,true);
-        Plateforme p4 = new Plateforme(Platform, 100, 600, 100, 700, 500,true);
-        Plateforme p5 = new Plateforme(Platform, 100, 600, 100, 900, 500,true);
-        Plateforme p6 = new Plateforme(Platform, 100, 600, 100, 800, 400,true);
-        Plateforme p7 = new Plateforme(Platform, 100, 600, 100, 700, 300,true);
+            ////JOUEUR
+            Image Skin = new Image(getClass().getClassLoader().getResource("image/testpers.png").toExternalForm());
+            Joueur j1 = new Joueur(Skin, 10, 10, 10, 0, 0,"Joueur1");
 
 
+            //ImageView joueurIV = new ImageView(getClass().getClassLoader().getResource("image/testpers.png").toExternalForm());
+            ////////////
+
+            //Image bg = new Image(getClass().getClassLoader().getResource("image/background.png").toExternalForm());
+
+            //s.setFill(Color.grayRgb(35));
+            ArrayList<Plateforme> plateformeArrayList = new ArrayList<Plateforme>();
+            ArrayList<Entite> entites = new ArrayList<Entite>();
+
+            Image Platform = new Image(getClass().getClassLoader().getResource("image/platform.png").toExternalForm());
+            Plateforme p1 = new Plateforme(Platform, 100, 600, 100, 100, 200, true);
+            Plateforme p2 = new Plateforme(Platform, 100, 600, 100,300, 500,true);
+            Plateforme p3 = new Plateforme(Platform, 100, 600, 100, 500, 500,true);
+            Plateforme p4 = new Plateforme(Platform, 100, 600, 100, 700, 500,true);
+            Plateforme p5 = new Plateforme(Platform, 100, 600, 100, 900, 500,true);
+            Plateforme p6 = new Plateforme(Platform, 100, 600, 100, 800, 400,true);
+            Plateforme p7 = new Plateforme(Platform, 100, 600, 100, 700, 300,true);
 
 
-        plateformeArrayList.add(p1);
-        plateformeArrayList.add(p2);
-        plateformeArrayList.add(p3);
-        plateformeArrayList.add(p4);
-        plateformeArrayList.add(p5);
-        plateformeArrayList.add(p6);
-        plateformeArrayList.add(p7);
-
-        entites.add(j1);
-        entites.add(p1);
-        entites.add(p2);
-        entites.add(p3);
-        entites.add(p4);
-        entites.add(p5);
-        entites.add(p6);
-        entites.add(p7);
 
 
-        for (Entite enti : entites) {
-            g.getChildren().add(enti.getIV());
-        }
+            plateformeArrayList.add(p1);
+            plateformeArrayList.add(p2);
+            plateformeArrayList.add(p3);
+            plateformeArrayList.add(p4);
+            plateformeArrayList.add(p5);
+            plateformeArrayList.add(p6);
+            plateformeArrayList.add(p7);
+
+            entites.add(j1);
+            entites.add(p1);
+            entites.add(p2);
+            entites.add(p3);
+            entites.add(p4);
+            entites.add(p5);
+            entites.add(p6);
+            entites.add(p7);
+
+
+            for (Entite enti : entites) {
+                g.getChildren().add(enti.getIV());
+            }
+            Text timerBind = new Text("0");
+            timerBind.setFill(Color.WHITE);
+            timerBind.setFont(Font.font(40));
+            timerBind.setTranslateX(600);
+            timerBind.setTranslateY(40);
+            g.getChildren().add(timerBind);
+
+
 
         Timer timer = new java.util.Timer();
 
-        Thread t = new Thread(() -> {
-            while(true){
-                Boolean exit = true;
-                timetmp++;
-                timejeu = timetmp/60;
+            Thread t = new Thread(() -> {
+                while(true){
+                    Boolean exit = true;
+                    timetmp++;
+                    timejeu = timetmp/60;
 
 
 
-                //AffichTimeHautEcran(timejeu.toString(), canvas);
+                    //AffichTimeHautEcran(timejeu.toString(), canvas);
 
-                javafx.application.Platform.runLater(() -> {
-                    timer.schedule(new TimerTask(){
-                        public void run(){
-                            AffichTime(timetmp);
-                        }
-                    }, 1000);
-                    ActionMouvement(action);
-                    action.ActionLectureListe(action, j1, stage, entites);
-                    collisionController.verify(plateformeArrayList, j1);
-                    mouvementJoueur.UpdateJoueur(j1);
+                    javafx.application.Platform.runLater(() -> {
+                        timer.schedule(new TimerTask(){
+                            public void run(){
+                                timerBind.setText(timejeu.toString());
+                                //AffichTime(timetmp);
+                            }
+                        }, 1000);
+                        ActionMouvement(action);
+                        action.ActionLectureListe(action, j1, stage, entites);
+                        collisionController.verify(plateformeArrayList, j1);
+                        mouvementJoueur.UpdateJoueur(j1);
 
-                });
+                    });
 
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
+            });
 
-        t.setDaemon(true);
-        t.start();
-        stage.show();
+            t.setDaemon(true);
+            t.start();
+            stage.show();
 
     }
 /*
