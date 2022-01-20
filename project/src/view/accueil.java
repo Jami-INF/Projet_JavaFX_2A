d65ca10001ad7036ec3f595ba5413e6338741372
Jami Infante <83696339+Jami-INF@users.noreply.github.com>
2022-01-20 19:05:27 +0100

    Ajout Binding liste temps+pseudos sur la page d'accueil

--- project/src/view/accueil.java
+++ project/src/view/accueil.java
@@ -1,21 +1,32 @@
 package view;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import model.manager;
 import model.persistance.IPersistanceTemps;
 import model.persistance.ISauvegardeTxt;
+import model.score;
 import view.game;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class accueil {
     @FXML
     private javafx.scene.control.Button Jouer;
     private Label labelAfficher;
     private model.manager manager;
+    //private TableView<score> lc;
+    private ObservableList<score> lc = FXCollections.observableArrayList();
+    private manager m = new manager(new ISauvegardeTxt());
+    //private List<score> lc = new ArrayList<score>();
     @FXML
     private javafx.scene.control.TextField Pseudo;
     /**
@@ -25,7 +36,7 @@ public class accueil {
     private void cliqueSurBoutonNewGame(){
         String pseudo = Pseudo.getText();
         //System.out.println(pseudo);
-        manager m = new manager(new ISauvegardeTxt());
+
         Stage stage = (Stage) Jouer.getScene().getWindow();
         stage.close();
         game g = new game();
@@ -41,5 +52,20 @@ public class accueil {
     private void cliqueSurBoutonQuitter(){
         Stage stage = (Stage) Quit.getScene().getWindow();
         stage.close();
+    }
+    @FXML
+    private javafx.scene.control.TableColumn tablePseudo;
+    @FXML
+    private javafx.scene.control.TableColumn tableTemps;
+    @FXML
+    private javafx.scene.control.TableView tableScore;
+    @FXML
+    private void initialize() {
+        lc = FXCollections.observableArrayList(m.getScoresParties());
+
+        tablePseudo.setCellValueFactory(new PropertyValueFactory<score, String>("Pseudo"));
+        tableTemps.setCellValueFactory(new PropertyValueFactory<score, String>("Temps"));
+        tableScore.setItems(lc);
+        //tableTemps.setCellValueFactory(cellData -> "qdzdqzdqs");
     }
 }
