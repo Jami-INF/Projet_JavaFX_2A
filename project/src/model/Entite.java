dc7d4bb13f5496d7a2032557e54de5fcf9d11cab
VincentMart <92922812+VincentMart@users.noreply.github.com>
2022-01-20 17:58:27 +0100

    commit de la docs

--- project/src/model/Entite.java
+++ project/src/model/Entite.java
@@ -8,19 +8,15 @@ import java.util.ArrayList;
 public abstract class Entite extends ImageView {
 
     private double vitesse = 300;
-
     private double gravite = 5;
-
     private double imageSize;
-
     private double x;
     private double y;
     private Image skin;
     private double velociteX;
     private double velociteY;
     private double time;
-
-    ImageView IV;
+    public ImageView IV;
 
     public Entite(Image Skin, double VelociteX, double VelociteY, double Size, double SpawnX, double SpawnY){
         IV = new ImageView(Skin);
